6531ae37e6e0af1de4521155ce43f39caa817856
elguille86 <g.rodriguez.p@hotmail.com>
2016-04-03 18:16:18 -0500

    Uso de Grid (12 Columnas)

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Demo de bootrap</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+   <link rel="stylesheet" href="css/style.css">
+   
+ 	
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+     <HEADER>
+       <div class="container">
+         <h1>Hola</hi>
+       </div>
+     </HEADER>
+     <!-- container-fluid -->
+     <div class="container"> 
+       <section class="main row">
+          <article class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+            <h3>Articulo - Article</h3>
+            <h4 class="visible-xs">12 Columnas (formato xs)</h4>
+            <h4 class="visible-sm">8 Columnas (formato sm)</h4>
+            <h4 class="visible-md">9 Columnas (formato md)</h4>
+            <h4 class="visible-lg">9 Columnas (formato lg)</h4>
+            <p>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+            Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, 
+            cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una 
+            galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. 
+            No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos 
+            electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la 
+            creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más 
+            recientemente con software de autoedición, como por ejemplo Aldus PageMaker, 
+            el cual incluye versiones de Lorem Ipsum.
+
+            Lorem
+            </p>
+          </article>
+          <aside class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <p>
+              <h3>Sidebar - Aside</h3>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+            Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, 
+            cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una 
+            galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. 
+            No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos 
+            electrónicos, quedando esencialmente igual al original.  
+            </p>
+          </aside>
+       </section>
+
+       <div class="row">
+         <div class="color1 col-mx-12 col-sm-6 col-md-3">
+           <p><h3>Columna</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perferendis doloribus aut explicabo suscipit ipsam blanditiis aliquam dolorum. Qui, voluptas suscipit consectetur ipsum, corporis quidem rerum harum aspernatur accusamus! Maiores!</p>          
+        </div>
+         <div class="col-mx-12 col-sm-6 col-md-3"><p><h3>Columna</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perferendis doloribus aut explicabo suscipit ipsam blanditiis aliquam dolorum. Qui, voluptas suscipit consectetur ipsum, corporis quidem rerum harum aspernatur accusamus! Maiores!</p></div>
+         
+         <!-- <div class="clearfix visible-sm-block"></div>  Alinea los Div al mismo Alto -->
+         <!-- Clase para movel a la Derecha los Margenes col-md-offset-3 -->
+         <div class="color1 col-mx-12 col-sm-6 col-md-3 "><p><h3>Columna</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perferendis doloribus aut explicabo suscipit ipsam blanditiis aliquam dolorum. Qui, voluptas suscipit consectetur ipsum, corporis quidem rerum harum aspernatur accusamus! Maiores!</p></div>
+         <div class="col-mx-12 col-sm-6 col-md-3"><p><h3>Columna</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perferendis doloribus aut explicabo suscipit ipsam blanditiis aliquam dolorum. Qui, voluptas suscipit consectetur ipsum, corporis quidem rerum harum aspernatur accusamus! Maiores!</p></div>
+       </div>
+     </div>
+     <footer>
+       <div class="container">Guillermo Rodriguez</div>
+     </footer>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
