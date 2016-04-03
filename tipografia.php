3bb0bd5fb76cf660ba70d7cdcf9642db4de26358
elguille86 <g.rodriguez.p@hotmail.com>
2016-04-03 18:19:30 -0500

    Uso de Tipografia , Colores y Etiquetas

---
+++ tipografia.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>bootrap - Tipografia</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+   
+ 	
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+ 
+     <!-- container-fluid -->
+     <div class="container-fluid"> 
+      <h1 class="text-muted">Este es un Hearder H1 <span class="small">Texto Secundario</span></h1>
+      <h2 class="text-primary">Este es un Hearder H2 <small>Texto Secundario</small></h2>
+      <h3 class="text-success">Este es un Hearder H3 <small>Texto Secundario</small></h3>
+      <h4 class="text-info">Este es un Hearder H4 <small>Texto Secundario</small></h4>
+      <h5 class="text-warning">Este es un Hearder H5 <small>Texto Secundario</small></h5>
+      <h6 class="text-danger">Este es un Hearder H6 <small>Texto Secundario</small></h6>
+
+
+      <h1 class="bg-primary">Este es un Hearder H1 <span class="small">Texto Secundario</span></h1>
+      <h2 class="bg-success">Este es un Hearder H2 <small>Texto Secundario</small></h2>
+      <h3 class="bg-info">Este es un Hearder H3 <small>Texto Secundario</small></h3>
+      <h4 class="bg-warning">Este es un Hearder H4 <small>Texto Secundario</small></h4>
+      <h5 class="bg-danger">Este es un Hearder H5 <small>Texto Secundario</small></h5>
+    
+    <small>Texto de Prueba</small>
+    <br/><br/>  
+    <div class="row">
+      <div class="col-md-4 text-left">
+        <p class="lead text-lowercase">CONVIERTO A MINISCULAS ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod </p>
+        <p class=""> <mark> tempore incididunt ut labore at dolore magna aliqua, Ut enim ad minim veriam,</mark></p>
+        <p class=""> <del> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod </del></p>
+        <p class=""> <s> consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </s> </p>
+        <p class=""> <ins> cillum dolore eu fugiat nulla pariatur. excepteru sint occaecat cupidatat non</ins></p>
+        <p class=""><u>Proident, sunt in culpa qui officia deserun mollit anim id est laborum</u></p>
+      </div>
+      <!-- text-justify -->
+      <div class="col-md-4 text-justify">
+        <p class="small text-uppercase">Lorem Convierto a mayusculas dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+        <p class=""> <strong>tempore incididunt ut labore at dolore magna aliqua, Ut enim ad minim veriam,</strong> </p>
+        <p class=""><b> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod</b></p>
+        <p class=""> <em> consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </em></p>
+        <p class="">cillum dolore eu fugiat nulla pariatur. excepteru sint occaecat cupidatat non</p>
+        <p class="">Proident, sunt in culpa qui officia deserun mollit anim id est laborum</p>
+      </div>
+
+      <div class="col-md-4 text-rigth">
+        <p class="text-capitalize">Ejemplo de Mostrar Codigo mira los etiquetas</p>
+        <p class=""><code>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod</code></p>
+        <p class=""><pre>consequat. Duis aute irure dolor in reprehenderit </pre></p>
+        <p class="">cillum dolore eu fugiat nulla pariatur. excepteru sint occaecat cupidatat non</p>
+        <p class="">Proident, sunt in culpa qui officia deserun mollit anim id est laborum</p>
+      </div>
+
+ 
+     </div>
+     <div class="row">
+       <blockquote class="blockquote-reverse">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sit ipsa consequuntur perspiciatis ab quae! Doloribus numquam exercitationem iusto totam ipsum est, vel maxime culpa aliquid. Neque non, magnam voluptate.</p>
+         <footer> Guillermo Rodriguez <cite tttle="Fuente">en Fuente.com</cite></footer>
+       </blockquote>
+
+     </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
